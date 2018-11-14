eddb50b23028386c60790bc13a3c243f2f915a58
fbl11 <freyab_london@yahoo.co.uk>
2018-11-14 23:15:22 +0000

    adds image to bullet class, working movement

--- src/bullet.js
+++ src/bullet.js
@@ -1,28 +1,28 @@
 class Bullet {
   constructor(center, velocity) { // is passed a horizontal and vertical center for the unit to determine spawn location - should be slightly outside the spawning unit's body
-    this.center = center
+    this.center = center // consists of this.center.x and this.center.y for horizontal and vertical position on the screen
     this.radius = 5
-    this.velocity = velocity
+    this.velocity = velocity // consists of this.velocity.x and this.verlocity.y for movement along the x axis and y axis
     this.color = 'green' // could be passed as argument on creation so colors can be changed on demand
   }
 
   update(canvas) {
-    this.center.x += this.velocity.dx  // adjusts horizontal position of center by incrementing current horizontal center position by horizontal velocity
-    this.center.y += this.velocity.dy // adjusts vertical position of center by incrementing current vertical center position by vertical velocity
+    if (this.center.x > 0) {
+      this.center.x += this.velocity.dx
+    }
+    // this.center.y += this.velocity.dy // not needed because our items only move along the x axis - adjusts vertical position of center by incrementing current vertical center position by vertical velocity
     this.draw(canvas)
   }
 
   draw(canvas) {
-    var ctx = canvas.getContext('2d') 
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    var ctx = canvas.getContext('2d')
+    let img = new Image();
+    img.src = ("./candycane.png")
+    ctx.clearRect(0, 0, canvas.width, canvas.height) // should probavly only be called by game.js
+    ctx.drawImage(img, this.center.x - 50, this.center.y + 50);
+    console.log(this.center.x)
+    console.log(this.center.y)
+    requestAnimationFrame(() => { this.update(canvas) }) // only for testing
 
-    console.log(canvas.height + 'height')
-    console.log(canvas.width + "width") // sets all pixels in the context (game board) from starting point (0, 0 - top left) and size (canvas width, canvas height) to transparent black, erasing previously drawn content - this removes trails  // should probably only be called by game.js
-    ctx.beginPath();
-    ctx.arc(this.center.x, this.center.y, this.radius, 0, Math.PI * 2, true);
-    ctx.fillStyle = this.color;
-    ctx.fill();
-    ctx.closePath();
-    requestAnimationFrame(() => { this.update(canvas) });
   }
 }
