41a0e2f1bed8a8adb1b7caebdc11f69c197298c8
fbl11 <freyab_london@yahoo.co.uk>
2018-11-15 11:22:26 +0000

    adds animation for candy cane

--- src/bullet.js
+++ src/bullet.js
@@ -1,9 +1,18 @@
 class Bullet {
   constructor(center, velocity) { // is passed a horizontal and vertical center for the unit to determine spawn location - should be slightly outside the spawning unit's body
-    this.center = center // consists of this.center.x and this.center.y for horizontal and vertical position on the screen
-    this.radius = 5
-    this.velocity = velocity // consists of this.velocity.x and this.verlocity.y for movement along the x axis and y axis
-    this.color = 'green' // could be passed as argument on creation so colors can be changed on demand
+    this.center = center; // consists of this.center.x and this.center.y for horizontal and vertical position on the screen
+    this.radius = 5;
+    this.velocity = velocity; // consists of this.velocity.x and this.verlocity.y for movement along the x axis and y axis
+    this.currentFrame = 0;
+    this.totalFrames = 4;
+    
+    this.spriteHeight = 100;
+    this.spriteWidth = 400 / 4;
+    this.srcX = 0;
+    this.srcY = 0;
+
+    this.animTick = 30;
+    this.currentAnimTick = 0;
   }
 
   update(canvas) {
@@ -17,12 +26,22 @@ class Bullet {
   draw(canvas) {
     var ctx = canvas.getContext('2d')
     let img = new Image();
-    img.src = ("./candycane.png")
+    img.src = ("./assets/candycane.png")
     ctx.clearRect(0, 0, canvas.width, canvas.height) // should probavly only be called by game.js
-    ctx.drawImage(img, this.center.x - 50, this.center.y + 50);
-    console.log(this.center.x)
-    console.log(this.center.y)
-    requestAnimationFrame(() => { this.update(canvas) }) // only for testing
+    if (this.currentAnimTick < this.animTick) {
+      this.currentAnimTick++;
+    } else {
+      if (this.currentFrame < this.totalFrames - 1) {
+        this.currentFrame++
+      } else {
+        this.currentFrame = 0
+      }
+      this.currentAnimTick = 0
+    }
+  
+    this.srcX = this.currentFrame * this.spriteWidth
+    ctx.drawImage(img, this.srcX, this.srcY, this.spriteWidth, this.spriteHeight, this.center.x, this.center.y, this.spriteWidth, this.spriteHeight);
 
+    requestAnimationFrame(() => { this.update(canvas) }) // only for testing
   }
 }
