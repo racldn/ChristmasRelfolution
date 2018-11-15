c8b8f2a899c268d8f7dc6c52ce3c6e9d51d17a3a
Nathan Quayle <nathan.quayle@gmail.com>
2018-11-15 18:49:36 +0000

    test branch

--- src/elf.js
+++ src/elf.js
@@ -1,21 +1,46 @@
 class Elf {
   constructor() {
-    this.canvas = document.getElementById("canvas");
+    this.canvas = document.getElementById("game-canvas");
     this.ctx = this.canvas.getContext("2d");
-    this.x = 50;
-    this.y = Math.floor(Math.random() * 6) * 100 + 50;
-    this.dx = 2;
+    this.x = 0;
+    this.y = Math.floor(Math.random() * 6) * 100;
+    this.dx = 0.3;
+
+    this.currentFrame = 0;
+    this.totalFrames = 2;
+
+    this.spriteHeight = 100;
+    this.spriteWidth = 146 / 2;
+    this.srcX = this.currentFrame * this.spriteWidth;
+    this.srcY = 0;
+
+    this.animTick = 30;
+    this.currentAnimTick = 0;
+
     this.draw();
   }
   
   draw() {
     let img = new Image();
-    img.src = ('./assets/elf.png');
-    this.ctx.drawImage(img, this.x, this.y);
+    img.src = ('./elf.png');
+
+    if (this.currentAnimTick < this.animTick) {
+      this.currentAnimTick++;
+    } else {
+      if (this.currentFrame < this.totalFrames - 1) {
+        this.currentFrame++;
+      } else {
+        this.currentFrame = 0;
+      }
+      this.currentAnimTick = 0;
+    }
+
+    this.srcX = this.currentFrame * this.spriteWidth;
+    this.ctx.drawImage(img, this.srcX, this.srcY, this.spriteWidth, this.spriteHeight, this.x, this.y, this.spriteWidth, this.spriteHeight);
   }
 
   update() {
-    if(this.x < this.canvas.width) {
+    if(this.x + this.spriteWidth < this.canvas.width) {
       this.x += this.dx;
     }
     this.draw();
--- src/game.js
+++ src/game.js
@@ -1,202 +1,167 @@
 class Game {
-    constructor() {
-        this.canvas = document.getElementById("canvas");
-        this.ctx = this.canvas.getContext("2d");
-        this.elves = [];
-        this.bullets = [];
-        this.weapons = [];
+	constructor() {
+		this.canvas = document.getElementById("canvas");
+		this.ctx = this.canvas.getContext("2d");
+		this.elves = [];
+		this.bullets = [];
+		this.weapons = [];
 
-        var html = document.body.parentNode;
+		this.canvasOffset = canvas.getBoundingClientRect();
 
-        this.htmlTop = html.offsetTop;
-        this.htmlLeft = html.offsetLeft;
+		this.valid = false;
+		this.dragging = false;
+		this.selection = null;
+		this.dragoffx = 0;
+		this.dragoffy = 0;
 
-        if (document.defaultView && document.defaultView.getComputedStyle) {
-            this.stylePaddingLeft = parseInt(document.defaultView.getComputedStyle(this.canvas, null)['paddingLeft'], 10) || 0;
-            this.stylePaddingTop = parseInt(document.defaultView.getComputedStyle(this.canvas, null)['paddingTop'], 10) || 0;
-            this.styleBorderLeft = parseInt(document.defaultView.getComputedStyle(this.canvas, null)['borderLeftWidth'], 10) || 0;
-            this.styleBorderTop = parseInt(document.defaultView.getComputedStyle(this.canvas, null)['borderTopWidth'], 10) || 0;
-        }
+		this.canvas.addEventListener('selectstart', (e) => {
+			e.preventDefault();
+			return false;
+		}, false);
 
-        this.valid = false;
-        this.dragging = false;
-        this.selection = null;
-        this.dragoffx = 0;
-        this.dragoffy = 0;
+		this.canvas.addEventListener('mousedown', (e) => {
+			var mouse = this.getMouse(e);
+			var mx = mouse.x;
+			var my = mouse.y;
+			var weapons = this.weapons;
+			var l = weapons.length;
+			for (var i = l - 1; i >= 0; i--) {
+				if (weapons[i].contains(mx, my)) {
+					var mySel = weapons[i];
+					this.dragoffx = mx - mySel.x;
+					this.dragoffy = my - mySel.y;
+					this.dragging = true;
+					this.selection = mySel;
+					this.valid = false;
+					return;
+				}
+			}
+			if (this.selection) {
+				this.selection = null;
+				this.valid = false;
+			}
+		}, true);
+		canvas.addEventListener('mousemove', (e) => {
+			if (this.dragging) {
+				var mouse = this.getMouse(e);
+				this.selection.x = mouse.x - this.dragoffx;
+				this.selection.y = mouse.y - this.dragoffy;
+				this.valid = false;
+			}
+		}, true);
+		canvas.addEventListener('mouseup', (e) => {
+			var mouse = this.getMouse(e);
+			var occupied = this.checkOccupied(mouse);
+			if (occupied == true) {
+				this.selection.x = this.selection.position.x;
+				this.selection.y = this.selection.position.y;
+				this.dragging = false;
+			} else {
+				var center = this.findCenter(mouse);
+				this.dragging = false;
+				this.selection.x = center.x - (this.selection.w / 2);
+				this.selection.y = center.y - (this.selection.h / 2);
+				this.selection.position = {
+					x: this.selection.x,
+					y: this.selection.y
+				}
+			}
+			this.valid = false;
+		}, true);
 
-        this.canvas.addEventListener('selectstart', (e) => {
-            e.preventDefault();
-            return false;
-        }, false);
-
-        this.canvas.addEventListener('mousedown', (e) => {
-            var mouse = this.getMouse(e);
-            var mx = mouse.x;
-            var my = mouse.y;
-            var weapons = this.weapons;
-            var l = weapons.length;
-            for (var i = l - 1; i >= 0; i--) {
-                if (weapons[i].contains(mx, my)) {
-                    var mySel = weapons[i];
-                    this.dragoffx = mx - mySel.x;
-                    this.dragoffy = my - mySel.y;
-                    this.dragging = true;
-                    this.selection = mySel;
-                    this.valid = false;
-                    return;
-                }
-            }
-            if (this.selection) {
-                this.selection = null;
-                this.valid = false;
-            }
-        }, true);
-        canvas.addEventListener('mousemove', (e) => {
-            if (this.dragging) {
-                var mouse = this.getMouse(e);
-                this.selection.x = mouse.x - this.dragoffx;
-                this.selection.y = mouse.y - this.dragoffy;
-                this.valid = false;
-            }
-        }, true);
-        canvas.addEventListener('mouseup', (e) => {
-            var mouse = this.getMouse(e);
-            var occupied = this.checkOccupied(mouse);
-            if (occupied == true) {
-                this.selection.x = this.selection.position.x;
-                this.selection.y = this.selection.position.y;
-                this.dragging = false;
-            } else {
-                var center = this.findCenter(mouse);
-                this.dragging = false;
-                this.selection.x = center.x - (this.selection.w / 2);
-                this.selection.y = center.y - (this.selection.h / 2);
-                this.selection.position = {
-                    x: this.selection.x,
-                    y: this.selection.y
-                }
-            }
-            this.valid = false;
-        }, true);
-
-        this.selectionColor = '#CC0000';
-        this.selectionWidth = 2;
-        this.interval = 30;
-        setInterval(() => {
-            this.draw();
-        }, this.interval);
+		this.selectionColor = '#CC0000';
+		this.selectionWidth = 2;
+		this.interval = 30;
+		setInterval(() => {
+			this.draw();
+		}, this.interval);
 
 
-        this.update();
-    }
+		this.update();
+	}
 
-    update() {
-        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+	update() {
+		this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-        this.elves.forEach((elf) => {
-            elf.update(this.ctx);
-        });
-        this.weapons.forEach((weapon) => {
-            weapon.update(this.ctx);
-        });
-        this.bullets.forEach((bullet) => {
-            bullet.update(this.ctx);
-        });
+		this.elves.forEach((elf) => {
+			elf.update(this.ctx);
+		});
+		this.weapons.forEach((weapon) => {
+			weapon.update(this.ctx);
+		});
+		this.bullets.forEach((bullet) => {
+			bullet.update(this.ctx);
+		});
 
-        requestAnimationFrame(() => {
-            this.update()
-        });
-    }
+		requestAnimationFrame(() => {
+			this.update()
+		});
+	}
 
-    addElf() {
-        this.elves.push(new Elf());
-    }
+	addElf() {
+		this.elves.push(new Elf());
+	}
 
-    addWeapon(weapon) {
-        this.weapons.push(weapon);
-        this.valid = false;
-    }
+	addWeapon(weapon) {
+		this.weapons.push(weapon);
+		this.valid = false;
+	}
 
-    getMouse(e) {
-        var element = this.canvas,
-            offsetX = 0,
-            offsetY = 0,
-            mx, my;
-        if (element.offsetParent !== undefined) {
-            do {
-                offsetX += element.offsetLeft;
-                offsetY += element.offsetTop;
-            } while ((element = element.offsetParent));
-        }
-        offsetX += this.stylePaddingLeft + this.styleBorderLeft + this.htmlLeft;
-        offsetY += this.stylePaddingTop + this.styleBorderTop + this.htmlTop;
+	getMouse(e) {
+		let mx = e.pageX - this.canvasOffset.top;
+		let my = e.pageY - this.canvasOffset.left;
 
-        mx = e.pageX - offsetX;
-        my = e.pageY - offsetY;
+		return {
+			x: mx,
+			y: my
+		};
+	}
 
-        return {
-            x: mx,
-            y: my
-        };
-    }
+	draw() {
+		if (!this.valid) {
+			this.weapons.forEach((weapon) => {
+				if (weapon.x > this.width || weapon.y > this.height ||
+					weapon.x + weapon.w < 0 || weapon.y + weapon.h < 0) return;
+				weapon.draw(this.ctx);
+			})
+			this.valid = true;
+		}
+	}
 
-    draw() {
-        if (!this.valid) {
-            var ctx = this.ctx;
-            var weapons = this.weapons;
-            var l = weapons.length;
-            for (var i = 0; i < l; i++) {
-                var weapon = weapons[i];
-                if (weapon.x > this.width || weapon.y > this.height ||
-                    weapon.x + weapon.w < 0 || weapon.y + weapon.h < 0) continue;
-                weapons[i].draw(ctx);
-            }
+	findCenter(mouse) {
+		if (mouse.x.toString().length == 2) {
+			var mx = 50;
+		} else {
+			var mx = parseInt(mouse.x.toString()[0]);
+			mx = (mx * 100) + 50;
+		}
 
-            if (this.selection != null) {
-                ctx.strokeStyle = this.selectionColor;
-                ctx.lineWidth = this.selectionWidth;
-                var mySel = this.selection;
-                ctx.strokeRect(mySel.x, mySel.y, mySel.w, mySel.h);
-            }
+		if (mouse.y.toString().length == 2) {
+			var my = 50;
+		} else {
+			var my = parseInt(mouse.y.toString()[0]);
+			my = (my * 100) + 50;
+		}
+		return {
+			x: mx,
+			y: my
+		};
+	}
 
-            this.valid = true;
-        }
-    }
-
-    findCenter(mouse) {
-      if (mouse.x.toString().length == 2) {
-        var mx = 50;
-      } else {
-        var mx = parseInt(mouse.x.toString()[0]);
-        mx = (mx * 100) + 50;
-      }
-
-      if (mouse.y.toString().length == 2) {
-        var my = 50;
-      } else {
-        var my = parseInt(mouse.y.toString()[0]);
-        my = (my * 100) + 50;
-      }
-      return {
-        x: mx,
-        y: my
-      };
-    }
-
-    checkOccupied(mouse) {
-        var mx = parseInt(mouse.x.toString()[0]);
-        var my = parseInt(mouse.y.toString()[0]);
-        var weapons = this.weapons;
-        var l = weapons.length;
-        for (var i = l - 1; i >= 0; i--) {
-            var mySel = weapons[i];
-            var gridX = parseInt(mySel.position.x.toString()[0]);
-            var gridY = parseInt(mySel.position.y.toString()[0]);
-            if (gridX == mx && gridY == my) {
-                return true;
-            }
-        }
-    }
+	checkOccupied(mouse) {
+		var mx = parseInt(mouse.x.toString()[0]);
+		var my = parseInt(mouse.y.toString()[0]);
+		var weapons = this.weapons;
+		var l = weapons.length;
+		for (var i = l - 1; i >= 0; i--) {
+			var mySel = weapons[i];
+			var gridX = parseInt(mySel.position.x.toString()[0]);
+			var gridY = parseInt(mySel.position.y.toString()[0]);
+			if (gridX == mx && gridY == my) {
+				return true;
+			}
+		}
+	}
 }
 
 const game = new Game();
@@ -207,5 +172,5 @@ game.addWeapon(new Weapon(700, 0, 100, 100, 'rgba(127, 255, 212, .5)'));
 game.addWeapon(new Weapon(700, 500, 100, 100, 'rgba(245, 222, 179, .7)'));
 
 setInterval(() => {
-    game.addElf(document.getElementById('canvas'));
+	game.addElf(document.getElementById('canvas'));
 }, 3000);
