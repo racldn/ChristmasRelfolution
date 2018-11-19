9071fa61f80614076a498b3459b359e287872309
Nathan Quayle <nathan.quayle@gmail.com>
2018-11-19 10:55:57 +0000

    added game dependency

--- src/collision.js
+++ src/collision.js
@@ -1,9 +1,9 @@
 collision = {
-	bulletHitsSide: (bullet) => {
+	bulletHitsSide: (bullet, game) => {
 		if(bullet.x <= 50) game.bullets.splice(game.bullets.indexOf(bullet), 1)
 	},
 
-	elfHitsWeapon: (elf) => {
+	elfHitsWeapon: (elf, game) => {
 		game.weapons.forEach((weapon) => {
 			if(elf.x + elf.spriteWidth >= weapon.x && elf.x + elf.spriteWidth <= weapon.x + weapon.w && elf.y == weapon.y) {
 				game.weapons.splice(game.weapons.indexOf(weapon), 1);
@@ -11,7 +11,7 @@ collision = {
 		});
 	},
 
-	elfHitsBullet: (elf) => {
+	elfHitsBullet: (elf, game) => {
 		game.bullets.forEach((bullet) => {
 			if(elf.x + elf.spriteWidth >= bullet.x && elf.x <= bullet.x && elf.y == bullet.y) {
 				game.elves.splice(game.elves.indexOf(elf), 1);
