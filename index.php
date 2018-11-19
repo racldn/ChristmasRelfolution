1a472566dbab39969ff5d8d0b081deb34781c528
Robbie Campbell <roberto251983@gmail.com>
2018-11-19 13:33:56 +0000

    heroku test2

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
 <?php include_once("index.html"); ?>
+<?php include_once("./src/collision.js"); ?>
+  <script src="./src/dragDrop.js"></script>
+  <script src="./src/snow.js"></script>
+  <script src="./src/sound.js"></script>
+  <script src="./src/game.js"></script>
+  <script src="./src/weapon.js"></script>
+  <script src="./src/gridCreation.js"></script>
+  <script src="./src/bullet.js"></script>
+  <script src="./src/elf.js"></script>
+  <script src="./src/button.js"></script>
+  <script src="./src/app.js"></script>
